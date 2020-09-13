63dc96ea7f97644a1939a5e1247f671b8e187f3b
Billy Grant <billygrant24@icloud.com>
2020-09-13 02:44:36 +0100

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      mono: [
+        '"Fira Mono"',
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace",
+      ],
+    },
+  },
+}
